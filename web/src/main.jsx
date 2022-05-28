e49393c511d22e51e68989248ed94761e3fb9fd0
Cristian Pérez Orozco <cristian.h.perez.o@gmail.com>
2022-05-28 20:11:58 +0100

    deleting rubbish

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -2,17 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-// redux
-import store from '../../common/redux/store'
-import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
 
     <App />
-
-    </Provider>
 
   </React.StrictMode>
 )
